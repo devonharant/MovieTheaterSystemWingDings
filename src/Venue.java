c354ec3aa942510ebc847276d7563e072eb98855
devonharant <59900157+devonharant@users.noreply.github.com>
2020-03-25 19:59:55 -0400

    skeleton for venue and children

--- src/Venue.java
+++ src/Venue.java
@@ -1,8 +1,29 @@
 /**
  * Venue abstract class to be implemented by children
- * forcing charactaristics and methods on them
+ * forcing characteristics and methods to be used
+ * by the children
  * @author WingDings
  */
 public abstract class Venue{
-
+	
+	protected String name;
+	protected String location;
+	protected String type;
+	//protected Show[] shows;
+	//protected Food[] food;
+	//protected Review review;
+	
+	public Venue() {
+		name = "";
+		location = "";
+		type = "";
+	}
+	public abstract String getName();
+	public abstract String getLocation();
+	public abstract String getType();
+	//public abstract Review getReview();
+	public abstract void setName(String name);
+	public abstract void setLocation(String location);
+	public abstract void printShows();
+	public abstract void printFood();
 }
