bb23642c5ee9d031bb0d738170cf8e3cd1e1e5e6
devonharant <59900157+devonharant@users.noreply.github.com>
2020-03-31 17:44:18 -0400

    adding functionality to show iterator

--- src/Cineplex.java
+++ src/Cineplex.java
@@ -12,22 +12,6 @@ public class Cineplex extends Venue{
 	}
 	
 	/**
-	 * getName method that returns the name of the venue
-	 * @return name
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * getLocation method that returns the location of the venue
-	 * @return location
-	 */
-	public String getLocation() {
-		return location;
-	}
-	
-	/**
 	 * getType method that returns the type of the venue
 	 * @return type
 	 */
@@ -35,44 +19,8 @@ public class Cineplex extends Venue{
 		return type;
 	}
 	
-	/**
-	 * returns the review
-	 */
-	public Review getReview() {
-		return null;
+	public void setType(String type) {
+		this.type = type;
 	}
 	
-	/**
-	 * setName method that sets the name of the specified venue
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * setLocation method that sets the location of the specified venue
-	 * @param location
-	 */
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	
-	/**
-	 * printShows method that prints the shows that are showing at this venue
-	 * using the show iterator
-	 * @return shows currently viewing
-	 */
-	public void printShows() {
-		
-	}
-	
-	/**
-	 * printFood method that prints the food currently available at this venue
-	 * using the food iterator
-	 * @return food at venue
-	 */
-	public void printFood() {
-		
-	}
 }
