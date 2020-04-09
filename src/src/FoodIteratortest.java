947ff07934557d8349fd746b6100a2880d24058c
canixon <canixon@users.noreply.github.com>
2020-04-09 17:41:01 -0400

    made test cases

---
+++ src/src/FoodIteratortest.java
@@ -0,0 +1,14 @@
+package src.src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FoodIteratortest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/src/ShowIteratortest.java
@@ -0,0 +1,14 @@
+package src.src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShowIteratortest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
