529d85f24f8fb32070ebbabe0411807c74ce5565
canixon <canixon@users.noreply.github.com>
2020-04-09 18:00:40 -0400

    user testing

--- src/src/UserTest.java
+++ src/src/UserTest.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	User user = new User(12, "chris", "nix", "11/16/1998", "noemail@gmail.com", "ca",
+			"password", 20, true);
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
 
+	public void testGetName() {
+		String name = "chris nix";
+		String producedName = user.getName();
+		assertEquals(name, producedName);
+	}
 }
