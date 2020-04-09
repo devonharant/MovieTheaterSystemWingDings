130bab777d83ee779d7a93bdaa86ca1bf32eaa7a
devonharant <59900157+devonharant@users.noreply.github.com>
2020-04-09 17:32:24 -0400

    creating junit test cases

---
+++ src/src/CineplexTest.java
@@ -0,0 +1,33 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+
+public class CineplexTest {
+	
+	Venue cineplex = new Cineplex("Test Plex", "USC");
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetType() {
+		
+	}
+	
+}
