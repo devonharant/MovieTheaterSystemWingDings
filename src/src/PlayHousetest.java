cf9ededd74cab4c9389f4c8b04dcff91ba5e162a
canixon <canixon@users.noreply.github.com>
2020-04-12 15:08:43 -0400

    updated playhousetest

--- src/src/PlayHousetest.java
+++ src/src/PlayHousetest.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class PlayHousetest {
 
+	Venue playhouse = new PlayHouse("house Test", "USC");
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
 
+	public void testGetType() {
+		String type = "Play House";
+		String produced = playhouse.getType();
+		assertEquals(type, produced);
+	}
 }
